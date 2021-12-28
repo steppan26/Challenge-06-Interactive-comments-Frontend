10e8eaf794d61b9a480fc14b83e37f6d297c12bc
Stephane Baroux <stephane.baroux@gmail.com>
2021-12-28 10:47:23 +0100

    moved the replies outside the comment component

--- src/javascript/components/comment.js
+++ src/javascript/components/comment.js
@@ -12,26 +12,6 @@ const Comment = ({ content = '', score = 0, user = {}, createdAt = '', replies =
         <p>{content}</p>
         <h4>{score}</h4>
       </div>
-      <ul className='replies'>
-        {
-          replies.length > 0 ?
-            replies.map(reply => {
-              return(
-                <div className='comment-list-item'>
-                  <div className="comment-header">
-                    <img src={reply.user.image.png} alt="user profile" />
-                    <h3 className='comment-username'>{reply.user.username}</h3>
-                    <h4 className='date-created'>{reply.createdAt}</h4>
-                  </div>
-                  <p>{reply.content}</p>
-                  <h4>{reply.score}</h4>
-                </div>
-              )
-            })
-          :
-          <div></div>
-        }
-      </ul>
     </div>
   )
 }
--- src/javascript/containers/comments_list.js
+++ src/javascript/containers/comments_list.js
@@ -13,6 +13,26 @@ const CommentsList = ({data}) => {
           return(
             <li key={comment.id}>
               <Comment content={comment.content} score={comment.score} user={comment.user} createdAt={comment.createdAt} replies={comment.replies} />
+              <ul className='replies'>
+                {
+                  comment.replies.length > 0 ?
+                    comment.replies.map(reply => {
+                      return (
+                        <div className='comment-list-item'>
+                          <div className="comment-header">
+                            <img src={reply.user.image.png} alt="user profile" />
+                            <h3 className='comment-username'>{reply.user.username}</h3>
+                            <h4 className='date-created'>{reply.createdAt}</h4>
+                          </div>
+                          <p>{reply.content}</p>
+                          <h4>{reply.score}</h4>
+                        </div>
+                      )
+                    })
+                    :
+                    <div></div>
+                }
+              </ul>
             </li>
           )
         })
